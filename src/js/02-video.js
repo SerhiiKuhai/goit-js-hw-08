e6796317ac27601c333b649eec36b0ee830924ce
SerhiiKuhai <sergk30@gmail.com>
2023-04-03 21:22:38 +0300

    3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,11 +4,11 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-const recordVideoplayerCurrentTime = throttle(function (time) {
+const recordVideoplayerCurrentTime = function (time) {
   localStorage.setItem('videoplayer-current-time', time.seconds);
-}, 1000);
+};
 
-player.on('timeupdate', recordVideoplayerCurrentTime);
+player.on('timeupdate', throttle(recordVideoplayerCurrentTime, 1000));
 
 const videoplayerCurrentTime = localStorage.getItem('videoplayer-current-time');
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,30 @@
 import throttle from 'lodash.throttle';
+
+const STORAGE_KEY = 'feedback-form-state';
+const input = document.querySelector('.feedback-form');
+const formData = {};
+
+input.addEventListener('submit', onFormSubmit);
+input.addEventListener('input', throttle(onInput, 500));
+
+(function populateInput() {
+  const formData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (formData) {
+    input.elements.email.value = formData.email || '';
+    input.elements.message.value = formData.message || '';
+  }
+})();
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function onInput(e) {
+  formData[e.target.name] = e.target.value;
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
